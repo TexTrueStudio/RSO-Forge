eb4cbd95c417c56147bf21810d32e8f0897ee0b7
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-01-03 16:25:16 +0800

    sync upstream

--- src/main/java/me/flashyreese/mods/reeses_sodium_options/ReesesSodiumOptions.java
+++ src/main/java/me/flashyreese/mods/reeses_sodium_options/ReesesSodiumOptions.java
@@ -1,4 +1,4 @@
-package me.flashyreese.mods.reeses_sodium_options.client;
+package me.flashyreese.mods.reeses_sodium_options;
 
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModList;
@@ -6,18 +6,14 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-//import net.minecraftforge.fml.network.FMLNetworkConstants;
 import net.minecraftforge.network.NetworkConstants;
-//import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(ReesesSodiumOptionsClientMod.MODID)
-public class ReesesSodiumOptionsClientMod {
+@Mod(ReesesSodiumOptions.MODID)
+public class ReesesSodiumOptions {
 
-    public static Logger LOGGER = LogManager.getLogger("Reese's Sodium Options");
-
-    private static String MOD_VERSION;
+    public static Logger LOGGER = LogManager.getLogger("TexTrue's Sodium Options");
 
     public static final String MODID = "reeses_sodium_options";
 
@@ -25,7 +21,7 @@ public class ReesesSodiumOptionsClientMod {
     public static boolean oculusLoaded;
 
 
-    public ReesesSodiumOptionsClientMod() {
+    public ReesesSodiumOptions() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onInitializeClient);
 
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
@@ -41,17 +37,9 @@ public class ReesesSodiumOptionsClientMod {
 
     public static Logger logger() {
         if (LOGGER == null) {
-            LOGGER = LogManager.getLogger("Reese's Sodium Options");
+            LOGGER = LogManager.getLogger("TexTrue's Sodium Options");
         }
 
         return LOGGER;
-    }
-
-    public static String getVersion() {
-        if (MOD_VERSION == null) {
-            throw new NullPointerException("Mod version hasn't been populated yet");
-        }
-
-        return MOD_VERSION;
     }
 }
