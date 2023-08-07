18b0d76834aadb288695e0cc3cfd735cebe31996
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-08-07 23:58:32 +0800

    update with upstream

--- src/main/java/me/flashyreese/mods/reeses_sodium_options/ReesesSodiumOptions.java
+++ src/main/java/me/flashyreese/mods/reeses_sodium_options/ReesesSodiumOptions.java
@@ -1,5 +1,7 @@
 package me.flashyreese.mods.reeses_sodium_options;
 
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -7,26 +9,19 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.network.NetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(ReesesSodiumOptions.MODID)
 public class ReesesSodiumOptions {
-
-    public static Logger LOGGER = LogManager.getLogger("TexTrue's Sodium Options");
-
     public static final String MODID = "reeses_sodium_options";
 
-    public static boolean rubidiumLoaded;
-    public static boolean oculusLoaded;
-
     public ReesesSodiumOptions() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onInitializeClient);
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> this::onInitClient);
     }
 
-    public void onInitializeClient(final FMLClientSetupEvent event) {
-        oculusLoaded = ModList.get().isLoaded("oculus");
-        rubidiumLoaded = ModList.get().isLoaded("rubidium");
+    private void onInitClient() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 }
