6cc651e31a594bd000db9d74527e3fa3ea23a7a1
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-08-08 00:02:21 +0800

    update with upstream

--- src/main/java/me/flashyreese/mods/reeses_sodium_options/ReesesSodiumOptions.java
+++ src/main/java/me/flashyreese/mods/reeses_sodium_options/ReesesSodiumOptions.java
@@ -1,5 +1,7 @@
 package me.flashyreese.mods.reeses_sodium_options;
 
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -10,6 +12,10 @@ public class ReesesSodiumOptions {
     public static final String MODID = "reeses_sodium_options";
 
     public ReesesSodiumOptions() {
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> this::onInitClient);
+    }
+
+    private void onInitClient() {
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 }
